22d7c80aae475d80fa41d782bc01e49bce7bbff4
陆佩 <peri.lu@mart.blue>
2021-04-01 18:43:42 +0800

    ...

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,10 +4,8 @@
 
 <script>
 export default {
-    name: 'HelloWorld',
-    props: {
-        msg: String
-    }
+    name: "HelloWorld",
+    props: {}
 };
 </script>
 
@@ -16,14 +14,17 @@ export default {
 h3 {
     margin: 40px 0 0;
 }
+
 ul {
     list-style-type: none;
     padding: 0;
 }
+
 li {
     display: inline-block;
     margin: 0 10px;
 }
+
 a {
     color: #42b983;
 }
--- src/main.js
+++ src/main.js
@@ -1,21 +1,21 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import {registerMicroApps, start} from 'qiankun';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import {registerMicroApps, start} from "qiankun";
 Vue.config.productionTip = false;
 registerMicroApps([
     {
-        name: 'app-react', // 微应用1名称
+        name: "app-react", // 微应用1名称
         entry: `http://${location.hostname}:8036`, // 微应用入口
-        container: '#container', //微应用容器节点的选择器
-        activeRule: '/app-react' // 微应用激活规则
+        container: "#container", //微应用容器节点的选择器
+        activeRule: "/app-react" // 微应用激活规则
     },
     {
-        name: 'app_1', // 微应用2名称
+        name: "app_1", // 微应用2名称
         entry: `http://${location.hostname}:8037`, // 微应用入口
-        container: '#container', //微应用容器节点的选择器
-        activeRule: '/app_1' // 微应用激活规则
+        container: "#container", //微应用容器节点的选择器
+        activeRule: "/app_1" // 微应用激活规则
     }
 ]);
 start();
@@ -25,4 +25,4 @@ new Vue({
     render: function (h) {
         return h(App);
     }
-}).$mount('#app');
+}).$mount("#app");
